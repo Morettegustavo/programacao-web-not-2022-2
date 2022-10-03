17f501d3fd39a0123b7fa78e24db6aabc9ed7748
Gustavo Faria Morette <88351614+Morettegustavo@users.noreply.github.com>
2022-10-03 07:58:09 -0300

    chore:add-project

---
+++ projeto_002/pages/exemplo001.jsx
@@ -0,0 +1,10 @@
+
+
+export default function init(){
+    return (
+        <>
+            <div className="azul">#azul</div>
+            <div className="verde">#verde</div>
+        </>
+    )
+}
---
+++ projeto_002/pages/exemplo002.jsx
@@ -0,0 +1,15 @@
+// se o css for global, ele deve ser importado no arquivo
+// _app.js (regra do next!!!)
+// incluir
+
+export default function init(){
+    return(
+        <>
+            <div className="001">
+                <div className="azul">#azul</div>
+                <div className="verde">#verde</div>
+            </div>
+        </>
+    )
+}
+
